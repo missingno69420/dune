5d84ece08cd5e08194d8daa52e9d7c41cff86776
missingno69420 <>
2025-05-29 01:44:29 -0400

    update query

--- main/models_ranked_by_solutions_submitted.sql
+++ main/models_ranked_by_solutions_submitted.sql
@@ -3,11 +3,6 @@ WITH models AS (
   SELECT id AS model
   FROM arbius_arbitrum.v2_enginev5_1_evt_modelregistered
 ),
-task_counts AS (
-  SELECT model, COUNT(*) as task_count
-  FROM arbius_arbitrum.v2_enginev5_1_evt_tasksubmitted
-  GROUP BY model
-),
 solution_counts AS (
   SELECT t.model, COUNT(*) as solution_count
   FROM arbius_arbitrum.v2_enginev5_1_evt_solutionsubmitted s
@@ -15,15 +10,9 @@ solution_counts AS (
   GROUP BY t.model
 )
 SELECT
-  CASE
-    WHEN LENGTH(COALESCE(mn.model_name, to_hex(m.model))) > 8
-    THEN SUBSTRING(COALESCE(mn.model_name, to_hex(m.model)) FROM 1 FOR 5) || '...'
-    ELSE COALESCE(mn.model_name, to_hex(m.model))
-  END AS model_name,
-  COALESCE(t.task_count, 0) as task_count,
+  COALESCE(mn.model_name, to_hex(m.model)) AS model_name,
   COALESCE(s.solution_count, 0) as solution_count
 FROM models m
-LEFT JOIN task_counts t ON m.model = t.model
 LEFT JOIN solution_counts s ON m.model = s.model
 LEFT JOIN query_5169304 mn ON m.model = mn.model_id
 ORDER BY solution_count DESC;
