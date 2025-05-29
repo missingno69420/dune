a35803ee7349173df38c58622fa18c6c4ec370e6
missingno69420 <>
2025-05-29 11:58:20 -0400

    update task info query

--- tasks/task_info.sql
+++ tasks/task_info.sql
@@ -1,114 +1,129 @@
+WITH all_events AS (
+-- TaskSubmitted: When the task was submitted
 SELECT
-  kind,
-  block_time,
-  task_id,
-  tx_hash,
-  evt_index,
-  task_fee,
-  model_owner_fee,
-  treasury_fee,
-  validator_fee,
-  treasury_reward,
-  task_owner_reward,
-  validator_reward,
-  incentive_paid
-FROM (
-  -- TaskSubmitted event
-  SELECT
-    'TaskSubmitted' AS kind,
-    t.evt_block_time AS block_time,
-    t.id AS task_id,
-    t.evt_tx_hash AS tx_hash,
-    t.evt_index AS evt_index,
-    t.fee AS task_fee,
-    NULL AS model_owner_fee,
-    NULL AS treasury_fee,
-    NULL AS validator_fee,
-    NULL AS treasury_reward,
-    NULL AS task_owner_reward,
-    NULL AS validator_reward,
-    NULL AS incentive_paid
-  FROM arbius_arbitrum.v2_enginev5_1_evt_tasksubmitted t
-  WHERE t.id = {{task_id}}
+'TaskSubmitted' AS kind,
+t.evt_block_time AS block_time,
+t.id AS task_id,
+t.evt_tx_hash AS tx_hash,
+t.evt_index AS evt_index,
+'fee: ' || CAST(t.fee AS VARCHAR) AS details
+FROM arbius_arbitrum.v2_enginev5_1_evt_tasksubmitted t
+WHERE t.id = {{task_id}}
 
-  UNION ALL
+UNION ALL
 
-  -- SolutionClaimed event from query_5179596
-  SELECT
-    'SolutionClaimed' AS kind,
-    p.block_time,
-    p.task_id,
-    p.tx_hash,
-    p.index AS evt_index,
-    NULL AS task_fee,
-    NULL AS model_owner_fee,
-    NULL AS treasury_fee,
-    NULL AS validator_fee,
-    NULL AS treasury_reward,
-    NULL AS task_owner_reward,
-    NULL AS validator_reward,
-    NULL AS incentive_paid
-  FROM query_5179596 p
-  WHERE p.task_id = {{task_id}} AND p.event_type = 'SolutionClaimed'
+-- SolutionSubmitted: When a solution was submitted for the task
+SELECT
+'SolutionSubmitted' AS kind,
+s.evt_block_time AS block_time,
+s.task AS task_id,
+s.evt_tx_hash AS tx_hash,
+s.evt_index AS evt_index,
+'submitted by: ' || CAST(s.addr AS VARCHAR) AS details
+FROM arbius_arbitrum.v2_enginev5_1_evt_solutionsubmitted s
+WHERE s.task = {{task_id}}
 
-  UNION ALL
+UNION ALL
 
-  -- FeesPaid event from query_5179596
-  SELECT
-    'FeesPaid' AS kind,
-    p.block_time,
-    p.task_id,
-    p.tx_hash,
-    p.index AS evt_index,
-    NULL AS task_fee,
-    p.model_owner_fee,
-    p.treasury_total_fee AS treasury_fee,
-    p.validator_fee,
-    NULL AS treasury_reward,
-    NULL AS task_owner_reward,
-    NULL AS validator_reward,
-    NULL AS incentive_paid
-  FROM query_5179596 p
-  WHERE p.task_id = {{task_id}} AND p.event_type = 'FeesPaid'
+-- SolutionClaimed: When a solution was claimed
+SELECT
+'SolutionClaimed' AS kind,
+s.evt_block_time AS block_time,
+s.task AS task_id,
+s.evt_tx_hash AS tx_hash,
+s.evt_index AS evt_index,
+'claimed by: ' || CAST(s.addr AS VARCHAR) AS details
+FROM arbius_arbitrum.v2_enginev5_1_evt_solutionclaimed s
+WHERE s.task = {{task_id}}
 
-  UNION ALL
+UNION ALL
 
-  -- RewardsPaid event from query_5179596
-  SELECT
-    'RewardsPaid' AS kind,
-    p.block_time,
-    p.task_id,
-    p.tx_hash,
-    p.index AS evt_index,
-    NULL AS task_fee,
-    NULL AS model_owner_fee,
-    NULL AS treasury_fee,
-    NULL AS validator_fee,
-    p.treasury_reward,
-    p.task_owner_reward,
-    p.validator_reward,
-    NULL AS incentive_paid
-  FROM query_5179596 p
-  WHERE p.task_id = {{task_id}} AND p.event_type = 'RewardsPaid'
+-- ContestationVote: When a validator voted on a contestation
+SELECT
+'ContestationVote' AS kind,
+v.evt_block_time AS block_time,
+v.task AS task_id,
+v.evt_tx_hash AS tx_hash,
+v.evt_index AS evt_index,
+'validator: ' || CAST(v.addr AS VARCHAR) || ', yea: ' || CAST(v.yea AS VARCHAR) AS details
+FROM arbius_arbitrum.v2_enginev5_1_evt_contestationvote v
+WHERE v.task = {{task_id}}
 
-  UNION ALL
+UNION ALL
 
-  -- IncentiveClaimed event
-  SELECT
-    'IncentiveClaimed' AS kind,
-    i.evt_block_time AS block_time,
-    i.taskid AS task_id,
-    i.evt_tx_hash AS tx_hash,
-    i.evt_index AS evt_index,
-    NULL AS task_fee,
-    NULL AS model_owner_fee,
-    NULL AS treasury_fee,
-    NULL AS validator_fee,
-    NULL AS treasury_reward,
-    NULL AS task_owner_reward,
-    NULL AS validator_reward,
-    i.amount AS incentive_paid
-  FROM arbius_arbitrum.arbiusrouterv1_evt_incentiveclaimed i
-  WHERE i.taskid = {{task_id}}
-) AS all_events
+-- ContestationVoteFinish: When the contestation voting concluded
+SELECT
+'ContestationVoteFinish' AS kind,
+f.evt_block_time AS block_time,
+f.id AS task_id,
+f.evt_tx_hash AS tx_hash,
+f.evt_index AS evt_index,
+'start_idx: ' || CAST(f.start_idx AS VARCHAR) || ', end_idx: ' || CAST(f.end_idx AS VARCHAR) AS details
+FROM arbius_arbitrum.v2_enginev5_1_evt_contestationvotefinish f
+WHERE f.id = {{task_id}}
+
+UNION ALL
+
+-- IncentiveAdded: When an incentive was added to the task
+SELECT
+'IncentiveAdded' AS kind,
+a.evt_block_time AS block_time,
+a.taskid AS task_id,
+a.evt_tx_hash AS tx_hash,
+a.evt_index AS evt_index,
+'amount: ' || CAST(a.amount AS VARCHAR) AS details
+FROM arbius_arbitrum.arbiusrouterv1_evt_incentiveadded a
+WHERE a.taskid = {{task_id}}
+
+UNION ALL
+
+-- FeesPaid: Fees distributed for the task
+SELECT
+'FeesPaid' AS kind,
+p.block_time,
+p.task_id,
+p.tx_hash,
+p.index AS evt_index,
+'model_owner_fee: ' || CAST(p.model_owner_fee AS VARCHAR) ||
+', treasury_fee: ' || CAST(p.treasury_total_fee AS VARCHAR) ||
+', validator_fee: ' || CAST(p.validator_fee AS VARCHAR) AS details
+FROM query_5179596 p
+WHERE p.task_id = {{task_id}} AND p.event_type = 'FeesPaid'
+
+UNION ALL
+
+-- RewardsPaid: Rewards distributed for the task
+SELECT
+'RewardsPaid' AS kind,
+p.block_time,
+p.task_id,
+p.tx_hash,
+p.index AS evt_index,
+'treasury_reward: ' || CAST(p.treasury_reward AS VARCHAR) ||
+', task_owner_reward: ' || CAST(p.task_owner_reward AS VARCHAR) ||
+', validator_reward: ' || CAST(p.validator_reward AS VARCHAR) AS details
+FROM query_5179596 p
+WHERE p.task_id = {{task_id}} AND p.event_type = 'RewardsPaid'
+
+UNION ALL
+
+-- IncentiveClaimed: When an incentive was claimed
+SELECT
+'IncentiveClaimed' AS kind,
+i.evt_block_time AS block_time,
+i.taskid AS task_id,
+i.evt_tx_hash AS tx_hash,
+i.evt_index AS evt_index,
+'amount: ' || CAST(i.amount AS VARCHAR) AS details
+FROM arbius_arbitrum.arbiusrouterv1_evt_incentiveclaimed i
+WHERE i.taskid = {{task_id}}
+)
+SELECT
+kind,
+block_time,
+task_id,
+tx_hash,
+evt_index,
+details
+FROM all_events
 ORDER BY block_time, evt_index;
