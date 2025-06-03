29eb1d6d1177972552604a0642e01edf9ea08c14
missingno69420 <>
2025-06-03 11:37:29 -0400

    Create cumulative rewards paid to vote escrow query

---
+++ validator_dashboard/cumulative_rewards_paid_vote_escrow.sql
@@ -0,0 +1,19 @@
+-- https://dune.com/queries/5228567
+WITH daily_ve_rewards AS (
+  SELECT
+    date_trunc('day', evt_block_time) AS day,
+    SUM(totalRewards) AS ve_rewards
+  FROM arbius_arbitrum.v2_enginev5_1_evt_rewardspaid
+  GROUP BY 1
+),
+cumulative_ve_rewards AS (
+  SELECT
+    day,
+    SUM(ve_rewards) OVER (ORDER BY day) AS cumulative_ve_rewards
+  FROM daily_ve_rewards
+)
+SELECT
+  day,
+  cumulative_ve_rewards / 1e18 AS cumulative_ve_rewards_tokens
+FROM cumulative_ve_rewards
+ORDER BY day
