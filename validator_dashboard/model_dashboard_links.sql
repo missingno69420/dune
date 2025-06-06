68359814f48ccee546fb00d9c868c8e2d84c30f7
missingno69420 <>
2025-06-05 20:44:37 -0400

    update model dashboard links query

--- validator_dashboard/model_dashboard_links.sql
+++ validator_dashboard/model_dashboard_links.sql
@@ -1,7 +1,7 @@
 -- https://dune.com/queries/5191539/
 SELECT
     get_href(
-        'https://dune.com/missingno69420/arbius-model-dashboard?model_id_t9cf92=0x' || model_id,
+        'https://dune.com/arbius_dashboards/model?model_id_tee4a4=0x' || model_id,
         CASE
             WHEN model_name IS NULL OR model_name = '' THEN '0x' || model_id
             ELSE model_name
