e5b95e2a2daafc8c5d4a2822e8936cb6878ae30b
missingno69420 <>
2025-06-06 12:13:43 -0400

    update v1 gysr rewards query

--- token_distribution/v1_gysr_rewards.sql
+++ token_distribution/v1_gysr_rewards.sql
@@ -1,4 +1,3 @@
--- https://dune.com/queries/5245571/
 WITH daily_sums AS (
   SELECT
     date_trunc('day', block_time) AS day,
@@ -8,10 +7,32 @@ WITH daily_sums AS (
     AND topic0 = 0x1a4dfb075362880d700ede1cc31d284b1c3b2811e9f0b2ddde7bdb270042c13f
     AND topic2 = 0x000000000000000000000000e3dbc4f88eaa632ddf9708732e2832eeaa6688ab
   GROUP BY 1
+),
+min_day AS (
+  SELECT MIN(day) AS first_day
+  FROM daily_sums
+),
+date_series AS (
+  SELECT time AS day
+  FROM (
+    SELECT sequence(
+      CAST((SELECT first_day FROM min_day) AS DATE),
+      CAST(date_trunc('day', NOW()) AS DATE),
+      INTERVAL '1' DAY
+    ) AS date_array
+  ) ts
+  CROSS JOIN UNNEST(date_array) AS t(time)
+),
+all_days AS (
+  SELECT
+    ds.day,
+    COALESCE(s.daily_amount, 0) AS daily_amount
+  FROM date_series ds
+  LEFT JOIN daily_sums s ON ds.day = s.day
 )
 SELECT
   day,
   CAST(daily_amount / POWER(10, 18) AS DECIMAL(38,18)) AS daily_amount,
   CAST(SUM(daily_amount) OVER (ORDER BY day) / POWER(10, 18) AS DECIMAL(38,18)) AS cumulative_amount
-FROM daily_sums
+FROM all_days
 ORDER BY day;
