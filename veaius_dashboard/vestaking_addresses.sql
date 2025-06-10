a55739e593a645b4dd427001d15902add2b05a9e
missingno69420 <>
2025-06-10 11:32:02 -0400

    update vestaking contracts

--- veaius_dashboard/vestaking_addresses.sql
+++ veaius_dashboard/vestaking_addresses.sql
@@ -1,4 +1,4 @@
 -- https://dune.com/queries/5228916
-select veStaking_ as address, call_block_number, call_tx_index
+select veStaking_ as address
 from arbius_arbitrum.engine_call_setvestaking
 order by call_block_number, call_tx_index DESC
