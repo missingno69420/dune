d0c62d552e9d4b3708972ed1236b43b1d63c4467
missingno69420 <>
2025-06-11 11:44:53 -0400

    update lp reward program fees

--- token_distribution/cumulative_rewards_paid_to_lp_rewards_protocols.sql
+++ token_distribution/cumulative_rewards_paid_to_lp_rewards_protocols.sql
@@ -17,6 +17,24 @@ v2_gysr_fee AS (
       AND topic0 = 0x6ded982279c8387ad8a63e73385031a3807c1862e633f06e09d11bcb6e282f60
       AND tx_hash = 0x0e7fa04f11df392bbf832056a4f70dd210b625267f9c9ce3715edd7edf7ce8d7
 ),
+merkl_fee_1 as (
+    SELECT
+        date_trunc('day', block_time) AS day,
+        varbinary_to_uint256(bytearray_substring(data, 1, 32)) AS fee_amount
+    FROM ethereum.logs
+        WHERE topic0 = 0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef
+        AND tx_hash = 0x8fc2a05ee85aca2c5b811a5c4fae238a7489211090257d8d99fc61a8eb3712cd
+        AND index = 242
+),
+merkl_fee_2 as (
+    SELECT
+        date_trunc('day', block_time) AS day,
+        varbinary_to_uint256(bytearray_substring(data, 1, 32)) AS fee_amount
+    FROM ethereum.logs
+        WHERE topic0 = 0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef
+        AND tx_hash = 0x4785ef911e59c8fd894b176a398e3bac80bd1a1e98c1184e534a0a6285b977f8
+        AND index = 643
+),
 combined_fees AS (
     SELECT
         day,
@@ -25,6 +43,10 @@ combined_fees AS (
         SELECT day, fee_amount FROM v1_gysr_fee
         UNION ALL
         SELECT day, fee_amount FROM v2_gysr_fee
+        UNION ALL
+        SELECT day, fee_amount FROM merkl_fee_1
+        UNION ALL
+        SELECT day, fee_amount FROM merkl_fee_2
     ) sub
     GROUP BY day
 ),
--- token_distribution/fees_paid_to_fund_lp_rewards.sql
+++ token_distribution/fees_paid_to_fund_lp_rewards.sql
@@ -1,3 +1,4 @@
+-- https://dune.com/queries/5267659
 WITH v1_gysr_fee as (
     SELECT
         date_trunc('day', block_time) AS day,
@@ -15,7 +16,29 @@ v2_gysr_fee as (
     WHERE contract_address = 0xA8f103eEcfb619358C35F98c9372B31c64d3f4A1
       AND topic0 = 0x6ded982279c8387ad8a63e73385031a3807c1862e633f06e09d11bcb6e282f60
       AND tx_hash = 0x0e7fa04f11df392bbf832056a4f70dd210b625267f9c9ce3715edd7edf7ce8d7
+),
+merkl_fee_1 as (
+    SELECT
+        date_trunc('day', block_time) AS day,
+        varbinary_to_uint256(bytearray_substring(data, 1, 32)) AS fee_amount
+    FROM ethereum.logs
+        WHERE topic0 = 0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef
+        AND tx_hash = 0x8fc2a05ee85aca2c5b811a5c4fae238a7489211090257d8d99fc61a8eb3712cd
+        AND index = 242
+),
+merkl_fee_2 as (
+    SELECT
+        date_trunc('day', block_time) AS day,
+        varbinary_to_uint256(bytearray_substring(data, 1, 32)) AS fee_amount
+    FROM ethereum.logs
+        WHERE topic0 = 0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef
+        AND tx_hash = 0x4785ef911e59c8fd894b176a398e3bac80bd1a1e98c1184e534a0a6285b977f8
+        AND index = 643
 )
 select day, 'Gysr' as payee, fee_amount / 1e18 as fee_amount from v1_gysr_fee
 union all
 select day, 'Gysr' as payee, fee_amount / 1e18 as fee_amount from v2_gysr_fee
+union all
+select day, 'Merkl' as payee, fee_amount / 1e18 as fee_amount from merkl_fee_1
+union all
+select day, 'Merkl' as payee, fee_amount / 1e18 as fee_amount from merkl_fee_2
